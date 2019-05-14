0daee8220ae53a4d3f170835eb2a90861c2e44e8
Rob Blackbourn <rblackbourn@ugsb-rbla01.bhdgsystematic.com>
2019-05-14 13:46:18 +0100

    Stuff

--- charts/react-chartjs-3/example1/src/components/YearOnYearChart.js
+++ charts/react-chartjs-3/example1/src/components/YearOnYearChart.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
+import { DateTime } from 'luxon'
+import { Line } from 'react-chartjs-3'
 import dornumAll from '../data/dornum-all'
 
 const styles = {
@@ -13,8 +15,82 @@ const styles = {
 
 class YearOnYearChart extends React.Component {
   render() {
-    const data2 = dornumAll.data.map(({ x, y }) => ({ x: new Date(x), y }))
-    console.log(data2)
+    const groupedByDay = dornumAll.data.reduce((obj, { x, y }) => {
+      const date = DateTime.fromMillis(x).toISODate()
+      if (!(date in obj)) {
+        obj[date] = []
+      }
+      obj[date].push({ x, y })
+      return obj
+    }, {})
+
+    console.log(groupedByDay)
+
+    const dailyIntermediate = Object.entries(groupedByDay).reduce(
+      (obj, [date, values]) => {
+        obj[date] = values.reduce(
+          ({ open, high, low, close, sum, sumsq, count }, { x, y }) => {
+            return {
+              open: open === null ? y : open,
+              high: high === null || y > high ? y : high,
+              low: low == null || y < low ? y : low,
+              close: y,
+              sum: sum + y,
+              sumsq: sumsq + y * y,
+              count: count + 1,
+            }
+          },
+          {
+            open: null,
+            high: null,
+            low: null,
+            close: null,
+            sum: 0,
+            sumsq: 0,
+            count: 0,
+          }
+        )
+        return obj
+      },
+      {}
+    )
+
+    console.log(dailyIntermediate)
+
+    const dailySummarized = Object.entries(dailyIntermediate).map(
+      ([date, { open, high, low, close, sum, sumsq, count }]) => ({
+        date: DateTime.fromISO(date, { zone: 'utc' }),
+        open,
+        high,
+        low,
+        close,
+        mean: count > 0 ? sum / count : null,
+        stdev:
+          count > 1
+            ? Math.sqrt((sumsq - (sum * sum) / count) / (count - 1))
+            : null,
+      })
+    )
+
+    console.log(dailySummarized)
+
+    const series = dailySummarized.reduce((obj, { date, mean }) => {
+      const year = date.year.toString()
+      if (!(year in obj)) {
+        obj[year] = []
+      }
+      obj[year].push({ x: date.toJSDate(), y: mean })
+      return obj
+    }, {})
+
+    console.log(series)
+
+    const datasets = Object.entries(series).map(([label, data]) => ({
+      label,
+      data,
+    }))
+
+    const data = { datasets }
 
     return (
       <div>
@@ -22,6 +98,14 @@ class YearOnYearChart extends React.Component {
           Year on year chart
         </Typography>
         <Typography variant="body2">This is some content</Typography>
+        <Line
+          data={data}
+          width={100}
+          height={50}
+          options={{
+            maintainAspectRatio: false,
+          }}
+        />
       </div>
     )
   }
